66163c6ca6bde1fe248326fde241d9f02ad5ac1a
Yousufjoy <kanekiken333@gmail.com>
2023-11-05 23:08:07 +0600

    Create Delete API and Delete a service Booking

--- index.js
+++ index.js
@@ -73,6 +73,15 @@ async function run() {
       res.send(result);
     });
 
+    // 5) Delete a single data
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
