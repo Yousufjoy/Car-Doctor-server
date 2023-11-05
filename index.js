de496e82bf978a6d93109dbb2ac49dfdee65a865
Yousufjoy <kanekiken333@gmail.com>
2023-11-05 13:41:26 +0600

    Insert bulk Data In Database and Load services data

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const port = process.env.port || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_PASS);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b9hatji.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,6 +25,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db("carDoctor").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find(); // find diye oi collection er document khujtesi jodi kichu thake tahole oita dibe na thakle empty cursor
+      const result = await cursor.toArray(); //toArray() iterate through the matching documents
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -33,7 +41,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
